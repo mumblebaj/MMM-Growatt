42b9dbd17d2c0854f205397c3c02001b487013af
mumblebaj <bernardmumble@gmail.com>
2023-02-11 16:41:41 +0200

    first commit

---
+++ MMM-Growatt.js
@@ -0,0 +1,66 @@
+Module.register("MMM-Growatt", {
+
+    defaults: {
+
+    },
+
+    getStyles: function() {
+        return ["MMM-Growatt.css"]
+    },
+
+    getTranslations: function() {
+
+    },
+
+    getTemplate() {
+
+    },
+
+    start: function() {
+        Log.info(`Starting module: ${this.name}`);
+
+        this.getGrowattData().then()
+        this.scheduleUpdate();
+    },
+
+    stop: function() {
+        Log.info('Stopping module ' + this.name);
+    },
+
+    resume: function() {
+        Log.info('Resuming module ' + this.name);
+        Log.debug('with config: ' + JSON.stringify(this.config));
+        this.suspend = false;
+        this.updateDom();
+    },
+
+    suspend: function() {
+        Log.info('Suspending module ' + this.name);
+        this.suspend = true;
+    },
+
+    getGrowattData: function() {
+        this.sendSocketNotification("GET_GROWATT_DATA", this.config)
+
+    },
+
+    scheduleUpdate: function(delay) {
+        var nextUpdate = this.config.updateInterval
+        if (typeof delay != "undefined" && delay >= 0) {
+            nextUpdate = delay
+        }
+
+        var self = this
+        setInterval(function() {
+            self.getGrowattData()
+        }, nextUpdate)
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        var self = this
+        if (notification === "GROWATT_DATA") {
+            this.growattData = payload
+            this.updateDom()
+        }
+    }
+})
---
+++ node_helper.js
@@ -0,0 +1,61 @@
+var NodeHelper = require('node_helper')
+var api = require('growatt')
+
+const options = {plantData:true,deviceData:false,weather:false,totalData:false,statusData:true,
+    deviceType:false,historyLast:true,historyAll:false}
+
+module.exports = NodeHelper.create({
+    requiresVersion: '2.21.0',
+
+    start: function() {
+        console.log('Starting node helper for ' + this.name)
+    },
+
+    deconstructPlantData(d, payload) {
+        plantDataFiltered = [];
+
+        const plantId = this.payload.plantId;
+
+        plantDataFiltered.push({
+            "plantName": d[plantId].plantName,
+            "country": d[plantId].plantData.country,
+            "city": d[plantId].plantData.city,
+            "accountName": d[plantId].plantData.accountName,
+            "inverterPower": d[plantId].plantData.nominalPower,
+            "treesSaved": d[plantId].plantData.tree,
+            "coalSaved": d[plantId].plantData.coal,
+            "ppv1": d[plantId].devices.RKG8CGL0JP.statusData.ppv1,
+            "ppv2": d[plantId].devices.RKG8CGL0JP.statusData.ppv2,
+            "gridPower": d[plantId].devices.RKG8CGL0JP.statusData.gridPower,
+            "discharging": d[plantId].devices.RKG8CGL0JP.historyLast.pBat,
+            "stateOfCharge": d[plantId].devices.RKG8CGL0JP.historyLast.capacity,
+            "consumptionPower": d[plantId].devices.RKG8CGL0JP.historyLast.outPutPower,
+            "rateVA": d[plantId].devices.RKG8CGL0JP.historyLast.rateVA
+        })
+        return plantDataFiltered;
+    },
+
+    getGrowattData: async function(payload) {
+        const growatt = new api({})
+        let login = await growatt.login(this.payload.user, this.payload.password).catch(e => {console.log(e)})
+        console.log('login: ',  login)
+        
+        let getAllPlantData = await growatt.getAllPlantData(options).catch(e => {console.log(e)})
+
+        var plantData = getAllPlantData;
+
+        var parserResponse = deconstructPlantData(plantData, payload)
+
+        var growattDataParsed = plantDataFiltered;
+
+        console.log(growattDataReturned);
+
+        this.sendSocketNotification('GROWATT_DATA', growattDataParsed)
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'GET_GROWATT_DATA') {
+            this.getGrowattData(payload)
+        }
+    }
+})
